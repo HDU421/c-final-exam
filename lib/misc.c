ff055d6e455824eff64dcc833ad542f1cd5a5268
Jimmy Zhang <codgician@users.noreply.github.com>
2017-11-10 10:49:41 +0800

    Initialize basic structure.

---
+++ lib/misc.c
@@ -0,0 +1,10 @@
+//
+// Created by codgician on 11/10/2017.
+//
+
+#include <stdlib.h>
+
+void pause()
+{
+    system("pause");
+}
---
+++ lib/misc.h
@@ -0,0 +1,10 @@
+//
+// Created by codgician on 11/10/2017.
+//
+
+#ifndef C_FINAL_EXAM_MISC_H
+#define C_FINAL_EXAM_MISC_H
+
+#endif //C_FINAL_EXAM_MISC_H
+
+void pause();
